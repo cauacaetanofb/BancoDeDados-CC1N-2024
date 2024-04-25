1bc3555ef476c59ec5315b32f977195bae995b13
cauacaetanofb <163075994+cauacaetanofb@users.noreply.github.com>
2024-04-25 20:51:59 -0300

    Add files via upload

---
+++ ALT TABLE.sql
@@ -0,0 +1,12 @@
+UPDATE pacientes 
+SET nome="Exemplo Gomes" 
+WHERE cpf_paciente="098.765.432-10";
+
+UPDATE consultas
+SET anamnese="textão"
+WHERE id_consultas="300"
+
+DELETE FROM consultas
+WHERE id_consultas = "200"
+
+select from * consultas;
